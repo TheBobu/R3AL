bc4dcc786a1669463a7836ac349f7e6cd5fa9ca8
Florescu Andrei <andrei.florescu.00@gmail.com>
2020-11-23 20:13:18 +0200

    route test

--- R3AL/ClientApp/src/app/app.module.ts
+++ R3AL/ClientApp/src/app/app.module.ts
@@ -7,19 +7,22 @@ import { RouterModule } from '@angular/router';
 import { AppComponent } from './app.component';
 import { NavMenuComponent } from './nav-menu/nav-menu.component';
 import { HomeComponent } from './home/home.component';
+import { QuizComponent } from './quiz/quiz.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavMenuComponent,
     HomeComponent,
+    QuizComponent
   ],
   imports: [
     BrowserModule.withServerTransition({ appId: 'ng-cli-universal' }),
     HttpClientModule,
     FormsModule,
     RouterModule.forRoot([
-      { path: '', component: HomeComponent, pathMatch: 'full' }
+      { path: '', component: HomeComponent, pathMatch: 'full' },
+      { path: 'quiz', component: QuizComponent }
     ])
   ],
   providers: [],
