b995a885b2282d9ebafde2e51e0ff3acbd9fc28c
Andreea-Maria Matei <59807073+DeeaMatei@users.noreply.github.com>
2020-11-23 20:25:32 +0200

    Add routing

--- R3AL/ClientApp/src/app/app.module.ts
+++ R3AL/ClientApp/src/app/app.module.ts
@@ -1,31 +1,40 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
-import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
-import { RouterModule } from '@angular/router';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
+import { FormsModule } from "@angular/forms";
+import { HttpClientModule, HTTP_INTERCEPTORS } from "@angular/common/http";
+import { RouterModule } from "@angular/router";
 
-import { AppComponent } from './app.component';
-import { NavMenuComponent } from './nav-menu/nav-menu.component';
-import { HomeComponent } from './home/home.component';
-import { QuizComponent } from './quiz/quiz.component';
+import { AppComponent } from "./app.component";
+import { NavMenuComponent } from "./nav-menu/nav-menu.component";
+import { HomeComponent } from "./home/home.component";
+import { QuizComponent } from "./quiz/quiz.component";
+import { GoalsComponent } from "./goals/goals.component";
+import { ProfileComponent } from "./profile/profile.component";
+import { ProjectsComponent } from "./projects/projects.component";
 
 @NgModule({
   declarations: [
     AppComponent,
     NavMenuComponent,
     HomeComponent,
-    QuizComponent
+    QuizComponent,
+    GoalsComponent,
+    ProfileComponent,
+    ProjectsComponent,
   ],
   imports: [
-    BrowserModule.withServerTransition({ appId: 'ng-cli-universal' }),
+    BrowserModule.withServerTransition({ appId: "ng-cli-universal" }),
     HttpClientModule,
     FormsModule,
     RouterModule.forRoot([
-      { path: '', component: HomeComponent, pathMatch: 'full' },
-      { path: 'quiz', component: QuizComponent }
-    ])
+      { path: "", component: HomeComponent, pathMatch: "full" },
+      { path: "quiz", component: QuizComponent },
+      { path: "goals", component: GoalsComponent },
+      { path: "profile", component: ProfileComponent },
+      { path: "projects", component: ProjectsComponent },
+    ]),
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
