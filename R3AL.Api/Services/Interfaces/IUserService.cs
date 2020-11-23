d637bb3114f16d394d272afa8bf6373fbdc45202
Florescu Andrei <andrei.florescu.00@gmail.com>
2020-11-23 21:08:43 +0200

    User Service implemented

--- R3AL.Api/Services/Interfaces/IUserService.cs
+++ R3AL.Api/Services/Interfaces/IUserService.cs
@@ -1,6 +1,17 @@
-﻿namespace R3AL.Core.Services.Interfaces
+﻿using R3AL.Data.Entities;
+
+namespace R3AL.Core.Services.Interfaces
 {
     public interface IUserService
     {
+        User AddUser(User user);
+
+        bool DeleteUser(int id);
+
+        User GetUserById(int id);
+
+        User GetUserByUsername(string username);
+
+        User UpdateUser(User user);
     }
 }
