d47fe0f424f40296058f3337ca045191c7978494
Florescu Andrei <andrei.florescu.00@gmail.com>
2020-11-23 14:16:12 +0200

    Add project files.

---
+++ R3AL.Data/Context.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace R3AL.Data
+{
+    public class Context
+    {
+    }
+}
---
+++ R3AL.Data/Entities/User.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace R3AL.Data.Entities
+{
+    public class User
+    {
+        public int UserId { get; set; }
+        public string Username { get; set; }
+        public string Password { get; set; }
+    }
+}
