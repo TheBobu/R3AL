8fa656a6c6387c0d66158f9b498ab2006dab03cf
Andreea-Maria Matei <59807073+DeeaMatei@users.noreply.github.com>
2020-11-24 16:18:33 +0200

    Add QuestionService

--- R3AL.Core/Services/Implementations/QuestionService.cs
+++ R3AL.Core/Services/Implementations/QuestionService.cs
@@ -1,5 +1,8 @@
-﻿using R3AL.Core.Services.Interfaces;
+﻿using System.Collections.Generic;
+using System.Linq;
+using R3AL.Core.Services.Interfaces;
 using R3AL.Data;
+using R3AL.Data.Entities;
 
 namespace R3AL.Core.Services.Implementations
 {
@@ -7,6 +10,54 @@ namespace R3AL.Core.Services.Implementations
     {
         public QuestionService(IContext context) : base(context)
         {
+        }
+
+        public Question AddQuestion(Question question)
+        {
+            question = Context
+                .Questions
+                .Add(question).Entity;
+
+            Context
+                .SaveAsync()
+                .ConfigureAwait(false);
+            return question;
+        }
+
+        public bool DeleteQuestion(int questionId)
+        {
+            Context.Questions.Remove(GetQuestionById(questionId));
+            Context
+                .SaveAsync()
+                .ConfigureAwait(false);
+            return true;
+        }
+
+        public Question GetQuestionById(int questionId)
+        {
+            return Context
+                .Questions
+                .Where(x => x.QuestionId.Equals(questionId))
+                .FirstOrDefault();
+        }
+
+        public IEnumerable<Question> GetQuestionsByQuizId(int quizId)
+        {
+            return Context
+                .Questions
+                .Where(x => x.QuizId.Equals(quizId))
+                .ToList();
+        }
+
+        public Question UpdateQuestion(Question question)
+        {
+            Context
+                .Questions
+                .Update(question);
+            Context
+                .SaveAsync()
+                .ConfigureAwait(false);
+            return question;
         }
     }
 }
--- R3AL.Core/Services/Interfaces/IQuestionService.cs
+++ R3AL.Core/Services/Interfaces/IQuestionService.cs
@@ -1,10 +1,16 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using R3AL.Data.Entities;
 
 namespace R3AL.Core.Services.Interfaces
 {
     public interface IQuestionService
     {
+        Question AddQuestion(Question question);
+        bool DeleteQuestion(int questionId);
+        Question GetQuestionById(int questionId);
+        IEnumerable<Question> GetQuestionsByQuizId(int quizId);
+        Question UpdateQuestion(Question question);
     }
 }
