7802642c6afe6f255bd7db8408edcc0d1544a05e
Florescu Andrei <andrei.florescu.00@gmail.com>
2020-11-24 15:04:51 +0200

    package.json

--- R3AL.Core/Manager/Implementations/GoalManager.cs
+++ R3AL.Core/Manager/Implementations/GoalManager.cs
@@ -1,8 +1,14 @@
 ﻿using R3AL.Core.Manager.Interfaces;
+using R3AL.Dtos;
+using System.Collections.Generic;
 
 namespace R3AL.Core.Manager.Implementations
 {
     public class GoalManager : IGoalManager
     {
+        public IEnumerable<GoalDto> GetGoals(int userId)
+        {
+            throw new System.NotImplementedException();
+        }
     }
 }
