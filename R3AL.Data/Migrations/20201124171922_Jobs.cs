ae11f876e431ccf2eba3f85f40a11847a3566999
Florescu Andrei <andrei.florescu.00@gmail.com>
2020-11-24 19:21:19 +0200

    Job Migration

---
+++ R3AL.Data/Migrations/20201124171922_Jobs.cs
@@ -0,0 +1,70 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace R3AL.Data.Migrations
+{
+    public partial class Jobs : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Job",
+                columns: table => new
+                {
+                    JobId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Description = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    ImgPath = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Job", x => x.JobId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "JobUser",
+                columns: table => new
+                {
+                    JobUserId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    UserId = table.Column<int>(type: "int", nullable: false),
+                    JobId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_JobUser", x => x.JobUserId);
+                    table.ForeignKey(
+                        name: "FK_JobUser_Job_JobId",
+                        column: x => x.JobId,
+                        principalTable: "Job",
+                        principalColumn: "JobId",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_JobUser_User_UserId",
+                        column: x => x.UserId,
+                        principalTable: "User",
+                        principalColumn: "UserId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_JobUser_JobId",
+                table: "JobUser",
+                column: "JobId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_JobUser_UserId",
+                table: "JobUser",
+                column: "UserId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "JobUser");
+
+            migrationBuilder.DropTable(
+                name: "Job");
+        }
+    }
+}
