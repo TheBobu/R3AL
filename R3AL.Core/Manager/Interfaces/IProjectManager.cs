a0a29080564f55e4249b62fb2a0f34c3bb447a84
Andreea-Maria Matei <59807073+DeeaMatei@users.noreply.github.com>
2020-11-24 22:05:36 +0200

    Create Project Manager and Controller

---
+++ R3AL.Core/Manager/Interfaces/IProjectManager.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using R3AL.Dtos;
+
+namespace R3AL.Core.Manager.Interfaces
+{
+    public interface IProjectManager
+    {
+        List<ProjectDto> GetProjectsByUserId(int userId);
+        ProjectDto GetProject(int projectId);
+
+        List<ProjectDto> GetProjectsByGoaldId(int goalId);
+    }
+}
