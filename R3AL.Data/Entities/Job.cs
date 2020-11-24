a2a291d775fc9f3a87be68528188f370be869853
Florescu Andrei <andrei.florescu.00@gmail.com>
2020-11-24 18:19:55 +0200

    Partial Goal Manager

---
+++ R3AL.Data/Entities/Job.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace R3AL.Data.Entities
+{
+    [Table(nameof(Job))]
+    public class Job
+    {
+        public int JobId { get; set; }
+        [Required]
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string ImgPath { get; set; }
+    }
+}
---
+++ R3AL.Data/Entities/JobUser.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace R3AL.Data.Entities
+{
+    [Table(nameof(JobUser))]
+    public class JobUser
+    {
+        public int JobUserId { get; set; }
+
+        [Required]
+        public int UserId { get; set; }
+
+        public User User { get; set; }
+
+        [Required]
+        public int JobId { get; set; }
+
+        public Job Job { get; set; }
+    }
+}
