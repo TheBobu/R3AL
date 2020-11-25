5dfca3c4f9c92b4e37c7d91d4d20904cdd96290b
andreeapurta <andreea.purta@gmail.com>
2020-11-25 13:37:44 +0200

    jobdescription final page

--- R3AL/ClientApp/src/app/joblist/joblist.component.ts
+++ R3AL/ClientApp/src/app/joblist/joblist.component.ts
@@ -8,22 +8,22 @@ import { getBaseUrl } from "src/main";
   styleUrls: ['./joblist.component.scss']
 })
 export class JoblistComponent {
-  public job : Joblist;
+  // public job : Joblist;
 
-constructor(http:HttpClient, @Inject('BASE_URL') baseUrl:string){
+// constructor(http:HttpClient, @Inject('BASE_URL') baseUrl:string){
   
-  http.get<Joblist>(baseUrl+"api/jobs/list").subscribe(result=>{
-    this.job= result;
-  }, error=>console.log(error));
-  }
+  // http.get<Joblist>(baseUrl+"api/jobs/1/list").subscribe(result=>{
+  //   this.job= result;
+  // }, error=>console.log(error));
+  // }
 }
-interface Joblist{
-  name: string;
-  numberOfProjects: number;
-  numberOfQuizes: number;
-  numberOfGoals: number;
-  status: string;
-  statusProgress: number;
-  milestones: number;
-}
+// interface Joblist{
+//   name: string;
+//   numberOfProjects: number;
+//   numberOfQuizes: number;
+//   numberOfGoals: number;
+//   status: string;
+//   statusProgress: number;
+//   milestones: number;
+// }
 
