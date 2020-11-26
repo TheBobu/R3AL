defd4d51fcde0b9126a48a7d3ab31532866cffcb
Florescu Andrei <andrei.florescu.00@gmail.com>
2020-11-26 13:55:07 +0200

    pdf viewer

--- R3AL/ClientApp/src/app/goals/goals.component.ts
+++ R3AL/ClientApp/src/app/goals/goals.component.ts
@@ -4,4 +4,20 @@ import { Component } from "@angular/core";
   selector: "app-goals",
   templateUrl: "./goals.component.html",
 })
-export class GoalsComponent {}
+export class GoalsComponent {
+
+  public pdfSrc: string;
+onFileSelected() {
+  let $img: any = document.querySelector('#file');
+
+  if (typeof (FileReader) !== 'undefined') {
+    let reader = new FileReader();
+
+    reader.onload = (e: any) => {
+      this.pdfSrc = e.target.result;
+    }; 
+
+    reader.readAsArrayBuffer($img.files[0]);
+  }
+}
+}
